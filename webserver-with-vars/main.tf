09bd0449ded915a52731db41aba212cf1ffad024
wangxu <wangxu@novastar.tech>
2024-01-10 10:17:41 +0800

    modify some errors

--- webserver-with-vars/main.tf
+++ webserver-with-vars/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_sercurity_group.instance.id]
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -34,7 +34,7 @@ resource "aws_security_group" "instance" {
 
   name = var.security_group_name
 
-  ingress = {
+  ingress  {
     from_port = var.server_port
     to_port = var.server_port
     protocol = "tcp"
