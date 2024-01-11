8aa5670aa1cd84cf671ca73e930c681e46ed473b
wangxu <wangxu@novastar.tech>
2024-01-11 16:27:02 +0800

    add tf moudles

--- moudles-tf/stage/data-stores/mysql/main.tf
+++ moudles-tf/stage/data-stores/mysql/main.tf
@@ -14,7 +14,7 @@ terraform {
 
     # bucket         = "<YOUR S3 BUCKET>"
     # key            = "<SOME PATH>/terraform.tfstate"
-    # region         = "us-east-2"
+    region         = "us-east-2"
     # dynamodb_table = "<YOUR DYNAMODB TABLE>"
     # encrypt        = true
   }
