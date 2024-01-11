24b50ee518484f538d1c8e0f24478088121dd17b
wangxu <wangxu@novastar.tech>
2024-01-11 18:01:20 +0800

    add tf moudles mysql80 allow_major_version_upgrade

--- moudles-tf/stage/data-stores/mysql/main.tf
+++ moudles-tf/stage/data-stores/mysql/main.tf
@@ -29,15 +29,15 @@ provider "aws" {
 resource "aws_db_instance" "example" {
   identifier_prefix   = "terraform-up-and-running"
   engine              = "mysql"
-  #engine_version       = "8.0"
-  engine_version       = "8.0.20"  # 可以指定特定的版本
+  engine_version       = "8.0"
+  # engine_version       = "8.0.20"  # 可以指定特定的版本
   allocated_storage   = 10
   instance_class      = "db.t2.micro"
   db_name             = var.db_name
   username            = var.db_username
   password            = var.db_password
   skip_final_snapshot = true
-  parameter_group_name = "default.mysql8.0.20"
+  parameter_group_name = "default.mysql8.0"
   allow_major_version_upgrade = true
   publicly_accessible = true
   vpc_security_group_ids = [aws_security_group.default.id]
