e9ed5b7170d8eb32bf3b81f96070ffe7d7f7409d
wangxu <wangxu@novastar.tech>
2024-01-11 11:02:46 +0800

    rds external access

--- rdsmysql-with-s3backend/stage/data-stores/mysql/main.tf
+++ rdsmysql-with-s3backend/stage/data-stores/mysql/main.tf
@@ -36,6 +36,7 @@ resource "aws_db_instance" "example" {
   db_name             = var.db_name
   username = var.db_username
   password = var.db_password
+  vpc_security_group_ids = [aws_security_group.default.id]
 }
 
 resource "aws_security_group" "default" {
